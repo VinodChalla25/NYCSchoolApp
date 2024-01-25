a45b68cd2695fbfd64c323411228519c3f71773c
RamohanReddy <ramohan.ios@gmail.com>
2024-01-25 23:37:03 +0530

    Added searchbar with filter

--- 20240119VinodNYCSchools/ViewController/SchoolListTableViewController.swift
+++ 20240119VinodNYCSchools/ViewController/SchoolListTableViewController.swift
@@ -9,16 +9,22 @@ import UIKit
 
 class SchoolListTableViewController: BaseViewController {
     
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     @IBOutlet private weak var tableView: UITableView!
     private var viewModel = SchoolListViewModel()
     private var listData:[SchoolDataModel]?
     
+    private var filterData:[SchoolDataModel]?
+    
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        searchBar.delegate = self
         loadData()
     }
+    
+    
     
     // MARK: - LoadData From API
     private func loadData() {
@@ -29,6 +35,7 @@ class SchoolListTableViewController: BaseViewController {
                     do{
                         guard let schooldata = try await self.viewModel.getList() else {return}
                         self.listData = schooldata
+                        self.filterData = schooldata
                         self.tableView.reloadData()
                         self.hideLoader()
                         
@@ -47,13 +54,15 @@ extension SchoolListTableViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return listData?.count ?? 0
+        return filterData?.count ?? 0
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-        cell.textLabel?.text = listData?[indexPath.row].schoolName
-        cell.detailTextLabel?.text = listData?[indexPath.row].dbn
+        let data = filterData?[indexPath.row]
+        cell.textLabel?.text = data?.schoolName
+        cell.detailTextLabel?.text = data?.dbn
         return cell
     }
 }
@@ -63,10 +72,30 @@ extension SchoolListTableViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let vc = storyboard?.instantiateViewController(identifier: "DetailSchoolViewcontoller") as? DetailSchoolViewcontoller {
             if let selectedSchoolData = listData?[indexPath.row] {
-                print("Selected School Name: \(selectedSchoolData.schoolName)")
+               // print("Selected School Name: \(selectedSchoolData.schoolName)")
                 vc.viewModel = selectedSchoolData
                 self.navigationController?.pushViewController(vc, animated: true)
             }
         }
+    }
+}
+
+extension SchoolListTableViewController:UISearchBarDelegate{
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        
+        
+        guard let data = listData else { return}
+        
+        filterData = searchText.isEmpty ? data : data.filter{
+            $0.schoolName.localizedCaseInsensitiveContains(searchText)
+        }
+        
+        filterData = filterData?.isEmpty ?? true ? data : filterData
+        
+        
+        tableView.reloadData()
+        
+        
     }
 }
